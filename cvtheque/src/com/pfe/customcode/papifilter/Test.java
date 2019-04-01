759b874c32cdc41b9da90d9abbfec1c5412629c8
meriam <meriam.sfaihi@gmail.com>
2019-04-01 21:49:34 +0200

    test github

--- cvtheque/src/com/pfe/customcode/papifilter/Test.java
+++ cvtheque/src/com/pfe/customcode/papifilter/Test.java
@@ -1,66 +1,47 @@
 package com.pfe.customcode.papifilter;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.zip.GZIPInputStream;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
 
 public class Test {
 
-	public static void main(String[] args) {
-		try {
-			String url = "https://api.stackexchange.com/2.2/users/1144035/answers?order=desc&sort=activity&site=stackoverflow&key=ocsWH7idlVKJNNQIKAeVSQ((";
-		     URL obj = new URL(url);
-		     HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-		     // optional default is GET
-		     con.setRequestMethod("GET");
-		     //add request header
-		     con.setRequestProperty("User-Agent", "Mozilla/5.0");
-		     
-		     
-		     int responseCode = con.getResponseCode();
-		     System.out.println("\nSending 'GET' request to URL : " + url);
-		     System.out.println("Response Code : " + responseCode);
-		     
-		     
-		     BufferedReader in = new BufferedReader(
-		             new InputStreamReader(con.getInputStream()));
-		     String inputLine;
-		     StringBuffer response = new StringBuffer();
-		     while ((inputLine = in.readLine()) != null) {
-		     	response.append(inputLine);
-		     }
-		     in.close();
-		     //print in String
-		     
-		     System.out.println(response.toString());
-		     //Read JSON response and print
-		     //JSONObject myResponse = new JSONObject(response.toString().replace("[", "").replace("]", ""));
-		     String resp =response.toString();
-		     JSONObject myResponse = new JSONObject(resp);
-		     JSONArray arr = new JSONArray();
-				arr = myResponse.getJSONArray("items");
-				String ownerLink = "";
-
-				if (arr.length() > 0) {
-					
-					 //ownerLink = arr.getJSONObject(i).getJSONObject("owner").getString("link");
-					ownerLink = arr.getJSONObject(0).getJSONObject("reputation").toString();
-					
-				
-				}
-	         System.out.println(ownerLink);
-	        } catch (Exception e) {
-	        	System.out.println("error ");
-	       }
-
-	}
-	
+	public static void main(String[] args) throws IOException, JSONException {
+		String str = parseRepos("https://api.github.com/users/AdamLindenthal");
+		System.out.println("repos:"+str);
 	     }
+public static String parseRepos(String url) throws IOException, JSONException{
+		
+		String result = parse(url);
+	    JSONObject myResponse = new JSONObject(result);  
+	    System.out.println(myResponse.toString());
+	    String reposUrl = myResponse.get("repos_url").toString();
+	    String result2 = parse(reposUrl);
+		JSONObject myResponse2 = new JSONObject(result2.substring(result2.indexOf('{')));
+	    return myResponse2.toString();	
+	}
+	public static String parse(String url) throws IOException, JSONException{
+		String baseURI = url;
+		ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config);
+		WebResource wr = client.resource(baseURI); 
+		ClientResponse response = null;
+		response = wr.get(ClientResponse.class);
+		String response_data = response.getEntity(String.class);
+
+		
+	    return response_data;
+	}
+}
 
 
