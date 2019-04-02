ab565c3493e2c9ad2790db57ed5fddb46b3953c2
meriam <meriam.sfaihi@gmail.com>
2019-04-02 19:03:58 +0200

    modif

--- cvtheque/src/com/pfe/customcode/papifilter/JsonReader.java
+++ cvtheque/src/com/pfe/customcode/papifilter/JsonReader.java
@@ -7,11 +7,16 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.json.JSONObject;
+
+import com.exalead.util.StringUtils;
 
 public class JsonReader {
 
@@ -24,12 +29,15 @@ public class JsonReader {
 		JSONObject reposResp = new JSONObject(myrepos.substring(myrepos.indexOf('{')));
 		System.out.println("repos!"+reposResp.toString());*/
 		
+		
+		//Boolean s = getMore("https://api.github.com/users/AdamLindenthal/repos?page=10&per_page=1");
+		//System.out.println(s);
 		String name = getReposName("https://api.github.com/users/AdamLindenthal");
 		System.out.println(name);
-		String desc = getReposDescription("https://api.github.com/users/AdamLindenthal");
-		System.out.println("des: "+desc);
-		String lang = getReposLangages("https://api.github.com/users/AdamLindenthal");
-		System.out.println("langages: "+lang);
+		//String desc = getReposDescription("https://api.github.com/users/AdamLindenthal");
+		//System.out.println("des: "+desc);
+		//String lang = getReposLangages("https://api.github.com/users/AdamLindenthal");
+		//System.out.println("langages: "+lang);
 		//String str = parse("https://api.github.com/users/AdamLindenthal/repos?page=1&per_page=1");
 		//System.out.println(str);
 		//String more = getMore("https://api.github.com/users/AdamLindenthal/repos?page=1&per_page=1");
@@ -54,7 +62,7 @@ public class JsonReader {
 		String link = myResponse.get("html_url").toString();
 		return link;
 	}
-	public static String getReposLangages(String url) throws Exception{
+	/*public static String getReposLangages(String url) throws Exception{
 		JSONObject jobj = new JSONObject();
 		jobj = getRepos(url);
 		
@@ -66,45 +74,85 @@ public class JsonReader {
 		jobj = getRepos(url);
 		//String name = jobj.get("name").toString();
 		return jobj.get("description").toString();
-	}
+	}*/
 	
 	public static String getReposName(String url) throws Exception{
 		JSONObject jobj = new JSONObject();
-		jobj = getRepos(url);
+		jobj = getMore(url);
 		//String name = jobj.get("name").toString();
 		return jobj.get("name").toString();
 	}
 	
 	
-	public static JSONObject getRepos(String url) throws Exception{
+	/*public static JSONObject getRepos(String url) throws Exception{
+		JSONObject reposResp = new JSONObject();
+		String resultat=null;
+		String link=null;
 		String res = parse(url);
 		JSONObject myResponse = new JSONObject(res);
 		String repos = myResponse.get("repos_url").toString();
-		System.out.println("url repos"+repos);
-		String myrepos = parse(repos+"?page=1&per_page=1");
-		JSONObject reposResp = new JSONObject(myrepos.substring(myrepos.indexOf('{')));
+		//System.out.println("url repos"+repos);
+		
 		
 		//String name = reposResp.get("name").toString();
 		//String description = reposResp.get("description").toString();
-
+		
+		
+		int p = 1;
+		if (getMore(repos)==true) {
+			link = repos+"?page="+p+"&per_page=1";
+			String myrepos = parse(link);
+			 reposResp = new JSONObject(myrepos.substring(myrepos.indexOf('{')));
+			p++;
+			
+		}
+		
 		
 		return reposResp;
-	}
+	}*/
 	
 	
 	
-	public static String getMore(String url) throws Exception {
-		URL obj = new URL(url);
+	public static JSONObject getMore(String url) throws Exception {
+		
+		JSONObject reposResp = new JSONObject();
+		String resultat=null;
+		String link=null;
+		String res = parse(url);
+		JSONObject myResponse = new JSONObject(res);
+		String repos = myResponse.get("repos_url").toString();
+		
+		
+				String[] Tableau2 = new String[2];
+				
+		URL obj = new URL(repos+"?page=10&per_page=1");
 		URLConnection conn = obj.openConnection();
 		
 		//get all headers
-		/*Map<String, List<String>> map = conn.getHeaderFields();
+		Map<String, List<String>> map = conn.getHeaderFields();
 		for (Map.Entry<String, List<String>> entry : map.entrySet()) {
 			System.out.println("Key : " + entry.getKey() + 
 	                 " ,Value : " + entry.getValue());
-		}*/
+		}
+		String str = conn.getHeaderField("Link");
+		String Tableau[] = str.split(","); 
 		
-		return conn.getHeaderField("Link");
+			int k = 0;
+			while (k<=Tableau.length-1) {
+				String text = Tableau[k];
+				
+				if (text.contains("next")==true) {
+					link = text.substring(1, text.indexOf(";")-1);
+					String myrepos = parse(link);
+					 reposResp = new JSONObject(myrepos.substring(myrepos.indexOf('{')));
+				}	
+					
+					
+					//resultat = parse(link);
+				
+				k++;
+			}
+		return reposResp;
 	}
 	
 	public static String parse(String purl) throws Exception {
@@ -113,6 +161,7 @@ public class JsonReader {
 	     HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 	     // optional default is GET
 	     con.setRequestMethod("GET");
+	     con.setRequestProperty("Authorization", "token 9cc26815ae56d43a7238535c8dbf48ddfa2c8af2");
 	     BufferedReader in = new BufferedReader(
 	             new InputStreamReader(con.getInputStream()));
 	     String inputLine;
